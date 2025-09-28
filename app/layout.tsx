54d7321870f1fd33b2e709dbac164969e5b31c4d
Mohammad8329 <mohammad.noorhasan.shaikh@nesswadiacollege.edu.in>
2025-09-28 20:05:16 +0530

    Initial clean project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "../components/header";
 
 
 export default function RootLayout({
@@ -9,11 +10,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>      
+      <html lang="en">
+        <body>
+          <Header/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
