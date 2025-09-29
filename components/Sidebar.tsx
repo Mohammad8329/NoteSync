89dbd151da540e8cfb156fbb40bf475af35bbed1
Mohammad8329 <mohammad.noorhasan.shaikh@nesswadiacollege.edu.in>
2025-09-29 07:01:16 +0530

    sidebar button added - changed some ui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,31 @@
+import { MenuIcon } from "lucide-react"
 import NewDocumentButton from "./NewDocumentButton"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 function Sidebar() {
   return (
-    <div className="p-2 md:p-5 bg-gray-500 relative">
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+        <Sheet>
+        <SheetTrigger><MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={20}/></SheetTrigger>
+        <SheetContent side="left">
+            <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <div>
+                {/* option */}
+            </div>
+            </SheetHeader>
+        </SheetContent>
+        </Sheet>
+        <div className= "hidden md:inline">         
         <NewDocumentButton />
+        </div>
     </div>
   )
 }
